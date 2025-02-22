3cb26b093b5cfe8c5b101939a56031f205a6a8a7
Ron Marche Rhyss Q. Uy <uyronmarcherhyssq@gmail.com>
2025-02-23 00:10:25 +0800

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        button: "rgba(45, 104, 248, 1)",
       },
     },
   },
